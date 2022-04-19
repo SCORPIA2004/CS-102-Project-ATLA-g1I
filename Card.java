4135ab2e4f5c11b856372e2112c954c0e1901547
ÖZGÜR İKİDAĞ <ozgur2dag@outlook.com>
2022-04-19 18:44:49 +0300

    commit classes 2

--- Card.java
+++ Card.java
@@ -1,5 +1,47 @@
-public class uu{
-    public static void main(String[] args) {
-        
+import java.awt.Image;
+
+public abstract class Card{
+    // variables
+
+    private boolean visibility;
+    private Instrument instrument;
+    private int id;
+    private int mana;
+    private Image cardImage;
+
+    //constructor
+    public Card(){
+        setId();
+
     }
+
+    //methods
+
+    public int getId(){
+        return this.id;
+    }
+
+    public int getMana(){
+        return this.mana;
+    }
+
+    public boolean isVisible(){
+        return this.visibility;
+    }
+    public void setId(){
+        this.id++;
+    }
+
+    public void setVisibility(boolean visibility){
+        this.visibility = visibility;
+    }
+
+    public void setInstrument(Instrument instr){
+        this.instrument = instr;
+    }
+
+    public void setCardImage(Image img){
+        this.cardImage = img;
+    }
+
 }
